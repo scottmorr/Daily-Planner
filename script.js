5856e46367abe9a0dfc91c8cbd073b7f5432df36
Scott Morrison <scottmorr75@gmail.com>
2020-03-26 10:59:11 -0600

    added table to html

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+ $(".saveBtn").on('click', function(){
+    var activity = $(this).sibling(".description").val();
+
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
