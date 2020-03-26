33cd793975df396923f63707bcb472a9f3949ef3
Scott Morrison <scottmorr75@gmail.com>
2020-03-26 14:42:01 -0600

    adding css

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
  $(".saveBtn").on('click', function(){
     var activity = $(this).sibling(".description").val();
-
-
- })
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, activity)
+ });
 
 
 
