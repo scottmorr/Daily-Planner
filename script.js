d641d233c13143d44972291245e178239bed8ae7
Scott Morrison <scottmorr75@gmail.com>
2020-03-27 12:02:38 -0600

    added time and date

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+
+//get save button to work
+//does saved info go to local storage
+//can I refresh page and activities stay
+//color code past present and future
+
+
+ var currentDay = moment().format("MMMM Do YYYY");
+$("#currentDay").append(currentDay);
+
+
+
+
  $(".saveBtn").on('click', function(){
     var activity = $(this).sibling(".description").val();
     var time = $(this).parent().attr("id");
@@ -7,26 +20,25 @@ $(document).ready(function() {
  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
+
+ //localStorage.setItem("key", "value");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
