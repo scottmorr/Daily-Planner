92b36233ed8411026cc79be76ca166695e8e9d18
Scott Morrison <scottmorr75@gmail.com>
2020-03-29 18:46:14 -0600

    finished local storage

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-//get save button to work
-//does saved info go to local storage
-//can I refresh page and activities stay
-//color code past present and future
+   //get save button to work
+   //does saved info go to local storage
+   //can I refresh page and activities stay
+   //color code past present and future
+
+   //added date
+   var currentDay = moment().format("MMMM Do YYYY");
+   $("#currentDay").append(currentDay);
 
 
- var currentDay = moment().format("MMMM Do YYYY");
-$("#currentDay").append(currentDay);
+   console.log('hi');
+
+   $(".saveBtn").on('click', function () {
+      var activity = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      window.localStorage.setItem(time, JSON.stringify(activity))
+      console.log(activity);
+      var item = localStorage.getItem(time, 'activity');
+      var parsed = JSON.parse(item);
+   });
+
+   //loop through hours      
+   for (var i = 0; i < 9; i++) {
+      document.getElementById('box-' + i).innerText = JSON.parse(localStorage.getItem('hour-' + i));
+
+   }
 
 
-
-
- $(".saveBtn").on('click', function(){
-    var activity = $(this).sibling(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, activity)
- });
-
-
-
-
-
-
-
-
- 
-})
-
-
- //localStorage.setItem("key", "value");
-
-
-
-
-
-
+   //for color coding save button
+   //for (var i = 0; i < 9; i++) {
+      $("button").click(function () {
+         $(".saveBtn").css("background-color", "yellow");
+      });
 
 
 
-
-
-
-
-
-
-
+   });
 
 
