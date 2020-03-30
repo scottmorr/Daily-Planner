a0673457607abb851c46577756fcd1378480dd1e
Scott Morrison <scottmorr75@gmail.com>
2020-03-30 11:41:27 -0600

    working on color coding

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ $(document).ready(function () {
    $("#currentDay").append(currentDay);
 
 
-   console.log('hi');
-
+   //local storage
    $(".saveBtn").on('click', function () {
       var activity = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
@@ -21,22 +20,54 @@ $(document).ready(function () {
       var item = localStorage.getItem(time, 'activity');
       var parsed = JSON.parse(item);
    });
+ 
+ //loop through hours and allows activities to be saved when page refreshes     
+ for (var i = 0; i < 9; i++) {
+   document.getElementById('box-' + i).innerText = JSON.parse(localStorage.getItem('hour-' + i));
 
-   //loop through hours      
-   for (var i = 0; i < 9; i++) {
-      document.getElementById('box-' + i).innerText = JSON.parse(localStorage.getItem('hour-' + i));
+}
+ 
+ 
+ 
+ 
+ 
+   //color codes page by time
+   function hourUpdater() {
+     var currentHour = moment().hours();
+     $(".time-block").each(function(){
+      var blockHour = parseInt($(this).attr("id").split("-")[1]) 
 
-   }
+      if(blockHour < currentHour) {
+         $(this).addClass("past");
+      }else if(blockHour === currentHour) {
+         $(this).removeClass("past");
+         $(this).addClass("present");
+      }else{
+         $(this).removeClass("past");
+         $(this).removeClass("present");
+         $(this).addClass("future");
+      }
+     })
+  }
+    hourUpdater();
+     var interval = setInterval(hourUpdater,15000);
+
+//   $("#hour-0 .description").val(localStorage.getItem("hour-0"));
+
+//   $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+//   $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+//   $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+//   $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+//   $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+//   $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+//   $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+//   $("#hour-8 .description").val(localStorage.getItem("hour-8"));
 
 
-   //for color coding save button
-   //for (var i = 0; i < 9; i++) {
-      $("button").click(function () {
-         $(".saveBtn").css("background-color", "yellow");
-      });
+   
 
 
+   
+});
 
-   });
-
-
+   
